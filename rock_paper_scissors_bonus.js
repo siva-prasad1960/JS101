895ceff04accd97407c445339ccc275dafa68345
Siva Prasad Konda <siva.sesi.59@gmail.com>
2021-04-27 00:06:46 +0530

    refactored RPS bonus file

--- rock_paper_scissors_bonus.js
+++ rock_paper_scissors_bonus.js
@@ -1,135 +1,101 @@
 const readline = require('readline-sync');
 const VALID_CHOICES = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
-const VALID_CHOICES_ALT = ['r', 'p', 's', 'l', 'sp'];
+const VALID_CHOICES_SHORTHAND = ['r', 'p', 's', 'l', 'sp'];
+const WIN_SCORE = 5;
+const WINNING_CHOICES = {
+  r: ['lizard', 'scissors'],
+  p: ['rock', 'spock'],
+  s: ['paper', 'lizard'],
+  l: ['spock', 'paper'],
+  sp: ['scissors', 'rock']
+};
 
 // function for user prompt + message.
 function prompt(message) {
   console.log(`=> ${message}`);
 }
 
-// function for deciding winner when user choice = rock.
-function winnerRock(computerChoice) {
-  prompt(`You chose ${VALID_CHOICES[0]}, computer chose ${computerChoice}`);
-  if (computerChoice === 'lizard' || computerChoice === 'scissors') {
-    return 'You win!';
-  } else if (computerChoice === 'paper' || computerChoice === 'spock') {
-    return 'Computer wins!';
-  } else {
-    return "It's a tie";
-  }
-}
-
-// function for deciding winner when user choice = paper.
-function winnerPaper(computerChoice) {
-  prompt(`You chose ${VALID_CHOICES[1]}, computer chose ${computerChoice}`);
-  if (computerChoice === 'rock' || computerChoice === 'spock') {
-    return 'You win!';
-  } else if (computerChoice === 'scissors' || computerChoice === 'lizard') {
-    return 'Computer wins!';
-  } else {
-    return "It's a tie";
-  }
-}
-
-// function for deciding winner when user choice = scissors.
-function winnerScissors(computerChoice) {
-  prompt(`You chose ${VALID_CHOICES[2]}, computer chose ${computerChoice}`);
-  if (computerChoice === 'paper' || computerChoice === 'lizard') {
-    return 'You win';
-  } else if (computerChoice === 'spock' || computerChoice === 'rock') {
-    return 'Computer wins';
-  } else {
-    return "It's a tie";
-  }
-}
-
-// function for deciding winner when user choice = lizard.
-function winnerLizard(computerChoice) {
-  prompt(`You chose ${VALID_CHOICES[3]}, computer chose ${computerChoice}`);
-  if (computerChoice === 'spock' || computerChoice === 'paper') {
-    return 'You win!';
-  } else if (computerChoice === 'rock' || computerChoice === 'scissors') {
-    return 'Computer wins!';
-  } else {
-    return "It's a tie";
-  }
-}
-
-
-// function for deciding winner when user choice = rock.
-function winnerSpock(computerChoice) {
-  prompt(`You chose ${VALID_CHOICES[4]}, computer chose ${computerChoice}`);
-  if (computerChoice === 'scissors' || computerChoice === 'rock') {
-    return 'You win!';
-  } else if (computerChoice === 'lizard' || computerChoice === 'paper') {
-    return 'Computer wins!';
-  } else {
-    return "It's a tie";
-  }
-}
-
-let userArr = [];
-let computerArr = [];
+let userScore = 0;
+let computerScore = 0;
 
 // function to keep win count for both user and computer.
-function winCount(finalCall) {
+function updateScore(finalCall) {
   while (finalCall.includes('You')) {
-    userArr.push('You');
+    userScore += 1;
     break;
   }
   while (finalCall.includes('Computer')) {
-    computerArr.push('Computer');
+    computerScore += 1;
     break;
   }
 }
 
-// function for checking if user or computer reaches to 5 wins first.
+//function to print tally at the end of each match.
+function displayTally() {
+  prompt(`Your score is: ${userScore}, computer score is ${computerScore}`);
+}
+
+// function for checking if user or computer reaches to WIN_SCORE wins first.
 function checkInWinner() {
-  if (userArr.length === 5) {
-    return "User is a grand winner, first to 5 wins!";
-  } else if (computerArr.length === 5) {
-    return "Computer is the grand winner, first to 5 wins!";
+  if (userScore === WIN_SCORE) {
+    return `User is a grand winner, first to ${WIN_SCORE} wins!`;
+  } else if (computerScore === WIN_SCORE) {
+    return `Computer is the grand winner, first to ${WIN_SCORE} wins!`;
   } else {
     return "";
   }
 }
 
+// Welcome prompt before the start of a fresh game.
+prompt(`Welcome to the game of ${VALID_CHOICES}, race to ${WIN_SCORE} between you and the computer.`);
 let answer;
 
 do {
-  prompt(`Choose one: ${VALID_CHOICES.join(', ')} or ${VALID_CHOICES_ALT.join(', ')}`);
+  prompt(`Choose one: ${VALID_CHOICES.join(', ')} or ${VALID_CHOICES_SHORTHAND.join(', ')}`);
   let choice = readline.question().toLowerCase();
 
   while (!VALID_CHOICES.includes(choice) &&
-        !VALID_CHOICES_ALT.includes(choice)) {
+        !VALID_CHOICES_SHORTHAND.includes(choice)) {
     prompt("Thats not a valid choice");
     choice = readline.question();
   }
 
-  let finalCall;
+  // statement to convert SHORTHAND choice to FULL choice names.
+  switch (choice) {
+    case 'r':
+      choice = 'rock';
+      break;
+    case 'l':
+      choice = 'lizard';
+      break;
+    case 'p':
+      choice = 'paper';
+      break;
+    case 's':
+      choice = 'scissors';
+      break;
+    case 'sp':
+      choice = 'spock';
+      break;
+    default:
+  }
+
   let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
   let computerChoice = VALID_CHOICES[randomIndex];
 
-  if (choice === 'rock' || choice === 'r') {
-    finalCall = winnerRock(computerChoice);
-    prompt(finalCall);
-    winCount(finalCall);
-  } else if (choice === 'paper' || choice === 'p') {
-    finalCall = winnerPaper(computerChoice);
-    prompt(finalCall);
-    winCount(finalCall);
-  } else if (choice === 'scissors' || choice === 's') {
-    finalCall = winnerScissors(computerChoice);
-    prompt(finalCall);
-    winCount(finalCall);
-  } else if (choice === 'lizard' || choice === 'l') {
-    finalCall = winnerLizard(computerChoice);
-    prompt(finalCall);
-    winCount(finalCall);
-  } else if (choice === 'spock' || choice === 'sp') {
-    finalCall = winnerSpock(computerChoice);
-    prompt(finalCall);
-    winCount(finalCall);
+  // winning logic.
+  if (WINNING_CHOICES[choice[0]].includes(computerChoice)) {
+    prompt(`You chose ${choice}, computer chose ${computerChoice}`);
+    prompt('You win!');
+    updateScore('You win!');
+    displayTally();
+  } else if (!WINNING_CHOICES[choice[0]].includes(computerChoice)) {
+    prompt(`You chose ${choice}, computer chose ${computerChoice}`);
+    prompt('Computer wins!');
+    updateScore('Computer Wins');
+    displayTally();
+  } else {
+    prompt("It's a tie");
   }
 
   let eureka = checkInWinner();
@@ -137,7 +103,7 @@ do {
   if (eureka === "") {
     prompt('Do you want to play again (y/n)?');
     answer = readline.question().toLowerCase();
-    while (answer[0] !== 'n' && answer[0] !== 'y') {
+    while (answer !== 'n' && answer !== 'y' && answer !== 'no' && answer !== 'yes') {
       prompt('Please enter "y" or "n".');
       answer = readline.question().toLowerCase();
     }
@@ -145,4 +111,4 @@ do {
     prompt(eureka);
     break;
   }
-} while (answer === 'y');
+} while (answer === 'y' || answer === 'yes');
