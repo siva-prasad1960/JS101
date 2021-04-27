aa4b72fbbea27666627f5317a09d9f8d4916d943
Siva Prasad Konda <siva.sesi.59@gmail.com>
2021-04-27 15:49:11 +0530

    lesson3 small problem set #3

---
+++ JS101_lesson3/object_key_check.js
@@ -0,0 +1,6 @@
+let ages = { Herman: 32, Lily: 30, Grandpa: 402, Eddie: 10 };
+
+console.log(ages.hasOwnProperty('Spot'));
+
+// checking for 'Spot' property in 'ages' object.
+// false, since ages object has no property named 'Spot'.
