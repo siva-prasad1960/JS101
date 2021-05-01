b43cd3b9ee5f231e3d8ed8365cc5c36acc160843
Siva Prasad Konda <siva.sesi.59@gmail.com>
2021-05-01 23:29:36 +0530

    small problems lesson3 set# 7

---
+++ JS101_lesson3/barney.js
@@ -0,0 +1,6 @@
+let flintstones = { Fred: 0, Wilma: 1, Barney: 2, Betty: 3, Bambam: 4, Pebbles: 5 };
+
+console.log(Object.entries(flintstones)); // result = nested array.
+
+console.log(Object.entries(flintstones).filter(pair => pair[0] === 'Barney').shift());
+// ['Barney', 2]
