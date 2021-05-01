2138fbded4e3504094b4ee777e0f86c60c672e43
Siva Prasad Konda <siva.sesi.59@gmail.com>
2021-05-01 16:18:17 +0530

    small problems lesson3 set# 4

---
+++ JS101_lesson3/infrontString.js
@@ -0,0 +1,9 @@
+let famousWords = "seven years ago...";
+
+console.log(`Four score and ${famousWords}`);
+
+let frontWords = "Four score and ";
+
+console.log(frontWords + famousWords);
+
+console.log("Four score and ".concat(famousWords));
