ac0c9768a01f484f3669c23e9ee4e3e91136bda6
Siva Prasad Konda <siva.sesi.59@gmail.com>
2021-05-07 19:44:46 +0530

    subArrayOrderedDescending.js

---
+++ JS101_lesson5/subArrayOrderedDescending.js
@@ -0,0 +1,19 @@
+let arr = [['b', 'c', 'a'], [2, 1, 3], ['blue', 'black', 'green']];
+
+let newArr = arr.map(subArr => {
+  return subArr.sort((a, b) => {
+    if (typeof subArr[0] === 'number') {
+      return b - a;
+    }
+
+    if (a < b) {
+      return 1;
+    } else if (a > b) {
+      return -1;
+    } else {
+      return 0;
+    }
+  });
+});
+
+console.log(newArr);
