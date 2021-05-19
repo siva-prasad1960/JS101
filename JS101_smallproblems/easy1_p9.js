088664f12908b9e5d43feba1e9443f6aa58ded14
Siva Prasad Konda <siva.sesi.59@gmail.com>
2021-05-19 20:22:28 +0530

    JS101 SP easy1 P9

---
+++ JS101_smallproblems/easy1_p9.js
@@ -0,0 +1,19 @@
+function multisum(inputNum) {
+  let arr = [];
+  for (let number = 1; number <= inputNum; number++) {
+    if (number % 3 === 0 && number % 5 === 0) {
+      arr.push(number);
+    } else if (number % 3 === 0) {
+      arr.push(number);
+    } else if (number % 5 === 0) {
+      arr.push(number);
+    }
+  }
+  return arr.reduce((acc, sum) => acc + sum);
+}
+
+console.log(multisum(3));
+console.log(multisum(5));
+console.log(multisum(10));
+console.log(multisum(1000));
+
